3d3624c933fb0a6342e1f3cabd7630aea553fc30
manish-infolanze <manish.infolanze@gmail.com>
2025-04-28 14:46:43 +0530

    testing

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
+// const express = require('express');
+// const { exec } = require('child_process');
+
+// const app = express();
+// app.use(express.json());
+
+// app.post('/git-webhook', (req, res) => {
+//     console.log('Webhook received!');
+//     exec('cd /path/where/you/want/hello-world && git pull', (error, stdout, stderr) => {
+//         if (error) {
+//             console.error(`Git Pull Error: ${error.message}`);
+//             return res.status(500).send('Git pull failed');
+//         }
+//         console.log(`Git Pull Output: ${stdout}`);
+//         res.status(200).send('Repository Updated');
+//     });
+// });
+
+// app.listen(3000, () => {
+//     console.log('Webhook server running on port 3000');
+// });
+
+// // CW5GI26R57BRATC3P2RQY7EWIUOJYUXK
+
+
+
 const express = require('express');
 const { exec } = require('child_process');
 
 const app = express();
 app.use(express.json());
 
+// 👉 New: GET request to show "Hello World" on home page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// 👉 This is still your webhook receiver
 app.post('/git-webhook', (req, res) => {
     console.log('Webhook received!');
     exec('cd /path/where/you/want/hello-world && git pull', (error, stdout, stderr) => {
@@ -16,8 +48,7 @@ app.post('/git-webhook', (req, res) => {
     });
 });
 
+// Server listen
 app.listen(3000, () => {
     console.log('Webhook server running on port 3000');
 });
-
-// CW5GI26R57BRATC3P2RQY7EWIUOJYUXK
